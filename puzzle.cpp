0731a0633e33894fcfb1c6d7c3915bd4df02a670
Dgprm <156766429+Dgprm@users.noreply.github.com>
2024-01-18 20:02:30 +0700

    Add files via upload

---
+++ puzzle.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+using namespace std;
+
+const int size = 3;
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define RESET   "\033[0m"
+
+void startGame(int puzzle[size][size]) {
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            if(puzzle[i][j] == 0) {
+                cout << "  ";
+            } else {
+                // Màu xanh lá cho số từ 1 đến 9
+                if (puzzle[i][j] >= 1 && puzzle[i][j] <= 9) {
+                    cout << GREEN << puzzle[i][j] << " " << RESET;
+                } else {
+                    cout << puzzle[i][j] << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
+
+void shuffleBoard(int puzzle[size][size]) {
+    srand(time(0));
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int randomI = rand() % 3;
+            int randomJ = rand() % 3;
+            swap(puzzle[i][j], puzzle[randomI][randomJ]);
+        }
+    }
+}
+
+bool isValidMove(int i, int j) {
+    return (i >= 0 && i < size && j >= 0 && j < size);
+}
+
+bool checkWin(int puzzle[size][size]) {
+    int counter = 1;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (puzzle[i][j] != counter % (size * size)) {
+                return false;
+            }
+            counter++;
+        }
+    }
+    return true;
+}
+
+void movePuzzle(int puzzle[size][size], int &emptyRow, int &emptyCol, char choice) {
+    int newRow = emptyRow;
+    int newCol = emptyCol;
+    switch(choice) {
+        case 'W':
+            newRow++;
+            break;
+        case 'S':
+            newRow--;
+            break;
+        case 'A':
+            newCol++;
+            break;
+        case 'D':
+            newCol--;
+            break;
+        default:
+            return;
+    }
+    if (isValidMove(newRow, newCol)) {
+        swap(puzzle[emptyRow][emptyCol], puzzle[newRow][newCol]);
+        emptyRow = newRow;
+        emptyCol = newCol;
+    }
+}
+
+void checkEmpty(int puzzle[size][size], int &emptyRow, int &emptyCol) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (puzzle[i][j] == 0) {
+                emptyRow = i;
+                emptyCol = j;
+            }
+        }
+    }
+}
+
+void autorunGame() {
+    int puzzle[size][size] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    int emptyRow, emptyCol;
+    char choice;
+    shuffleBoard(puzzle);
+    checkEmpty(puzzle, emptyRow, emptyCol);
+
+    do {
+        system("cls");
+        startGame(puzzle);
+        cout << RED << "Nhap W: di chuyen len, S: di chuyen xuong, A: di chuyen sang trai, D: di chuyen sang phai, Q de thoat game: ";
+        cin >> choice;
+        if (islower(choice)) {
+            choice = toupper(choice);
+        }
+        if (choice == 'Q') {
+            break;
+        }
+        movePuzzle(puzzle, emptyRow, emptyCol, choice);
+        if (checkWin(puzzle)) {
+            cout << "Xin chuc mung ban da chien thang";
+            break;
+        }
+    } while (true);
+}
+
+int main() {
+    autorunGame();
+    return 0;
+}
